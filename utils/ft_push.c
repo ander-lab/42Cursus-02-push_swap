653be0725f92b03782af2a06e4eb68f5c4fb45d3
Alex <jimenezs.alex@gmail.com>
2021-11-03 17:40:34 +0100

    pa & pb

--- utils/ft_push.c
+++ utils/ft_push.c
@@ -6,13 +6,22 @@
 /*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 12:49:44 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/11/03 13:51:53 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/11/03 17:26:40 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	ft_push(t_stack *src, t_stack **dst, char c)
+void	ft_push(t_stack **src, t_stack **dst, char c)
 {
+	t_stack *aux;
 
+	if (!*dst)
+		*dst = ft_lstnew_int((*src)->content);
+	else if (*dst)
+		ft_lstadd_front_int(dst, ft_lstnew_int((*src)->content));
+	aux = *src;
+	*src = aux->next;
+	free(aux);
+	ft_putmov('p', c, 'x');
 }
