08a608a9cbc2d95bcea0c98d59eabed436b85f3d
Alex <jimenezs.alex@gmail.com>
2021-11-03 17:42:56 +0100

    print

--- tmp/ft_print_stacks.c
+++ tmp/ft_print_stacks.c
@@ -6,7 +6,7 @@
 /*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 09:58:46 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/11/03 12:53:42 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/11/03 17:37:57 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 
 void	ft_print_stack(t_stack *stack_a, t_stack *stack_b)
 {
+	printf("\nstack a------->");
 	while (stack_a)
 	{
-		printf(" %i        %i\n", stack_a->content, stack_b->content);
+		printf("%i", stack_a->content);
 		stack_a = stack_a->next;
 	}
-	printf("-------	-------\n a	 b\n");
+	printf("\nstack b------->");
+	while (stack_b)
+	{
+		printf("%i", stack_b->content);
+		stack_b = stack_b->next;
+	}
+	printf("\n");
 }
