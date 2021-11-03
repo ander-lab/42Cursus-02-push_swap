59cf49a2a9da6d84f55f79852bce2f5a46f0cb08
Alex <jimenezs.alex@gmail.com>
2021-11-03 13:46:41 +0100

    ft_ss added

--- utils/ft_swap.c
+++ utils/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 10:39:39 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/11/03 12:56:49 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/11/03 13:45:56 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,11 @@ void	ft_swap(t_stack **stack, char c)
 	aux_begin->next = (*stack)->next;
 	(*stack)->next = aux_begin;
 	ft_putmov('s', c, 'x');
+}
+
+void	ft_ss(t_stack **stack_a, t_stack **stack_b)
+{
+	ft_swap(stack_a,'x');
+	ft_swap(stack_b, 'x');
+	write (1, "ss\n", 3);
 }
