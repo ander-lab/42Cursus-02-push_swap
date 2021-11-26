a1d3c79501996f2757868d019d59957e989769d0
Alex <jimenezs.alex@gmail.com>
2021-11-26 10:09:13 +0100

    errors.c file

---
+++ srcs/errors.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/26 09:56:04 by ajimenez          #+#    #+#             */
+/*   Updated: 2021/11/26 10:08:15 by ajimenez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+void	check_errors(t_stack *stack)
+{
+	if (/*TODO Funcion que compruebe que todos son int*/)
+
+	if (/*TODO Funcion que compruebe que va ordenada*/)
+
+}
--- srcs/parser.c
+++ srcs/parser.c
@@ -6,15 +6,14 @@
 /*   By: ajimenez <ajimenez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 12:07:03 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/11/24 17:49:52 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/11/26 09:55:56 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //TODO Funcion main que compruebe todos los posibles errores en los args
-	//TODO -----> ERRORES: Numeros repetidos
+	//TODO -----> ERRORES: Numeros repetido
 	//TODO -----> ERRORES: Is digit pero si hay un simbolo hay que saltarselo (atoi is digit?)
 	//TODO -----> ERRORES: Letras en medio de numeros
-//TODO Funcion para splitear los args del main y meterlos en la lista
 
 #include "../includes/push_swap.h"
 
